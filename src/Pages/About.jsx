f26bc489be76623c2d0e26f295f682bb88334494
Mario <mario.lauudi@dci-student.org>
2024-09-23 15:04:12 +0200

    U3

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,77 +3,56 @@ import React from "react";
 function About() {
   return (
     <div>
-      <h1>Urnengarten-Goldenbek</h1>
-      <h2>Frieden findet man nur in den Wäldern.</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
-        distinctio, quam officia odit neque harum consequatur aperiam
-        necessitatibus nisi voluptatum culpa, doloremque nemo, possimus
-        dignissimos exercitationem voluptas fugiat. Autem quaerat mollitia
-        asperiores cumque laudantium illum voluptas id fugit ab unde!
-        Exercitationem earum magnam sit, neque temporibus iste nulla assumenda
-        ratione totam obcaecati incidunt numquam fuga, dolorum enim! Vitae
-        animi, enim rerum laborum hic nostrum quisquam dolorum sint, culpa
-        molestias saepe? Minus voluptate obcaecati dolorum cumque impedit
-        consectetur officiis doloribus. Ratione autem corporis perspiciatis
-        consequuntur labore, laborum excepturi vitae qui neque amet et
-        temporibus voluptates cum provident odio atque ut quos itaque!
-        Consectetur repellat ut dolorum, atque natus voluptates quod odit
-        asperiores, totam fugiat autem, illum iste voluptate veniam optio
-        voluptatem? Nisi esse reprehenderit error repudiandae nam, modi ad
-        fugit. Beatae dolorem ipsa aperiam omnis vitae iure illo, dolorum, sed,
-        nesciunt reiciendis ab totam expedita recusandae voluptatem nulla hic
-        ullam magni enim! Iusto dolores quaerat voluptatem voluptas accusamus
-        dicta vero aspernatur neque odio nisi, provident labore animi in
-        consectetur quae id voluptate recusandae delectus eius quisquam totam
-        placeat ipsam. Unde consectetur assumenda tenetur eligendi odio ab,
-        maiores ipsam numquam quibusdam ipsum debitis eveniet exercitationem
-        incidunt laboriosam ea, vero, mollitia accusantium obcaecati doloremque?
-        Delectus neque accusantium laudantium amet aspernatur qui natus maxime
-        sunt vero numquam, culpa aliquid excepturi quam tenetur, voluptatum
-        doloribus nulla aperiam a porro expedita suscipit esse? Sequi
-        dignissimos ullam alias omnis, at qui culpa vitae vel facilis eius,
-        reprehenderit iure nam sapiente repellat neque! At iusto deleniti fugiat
-        suscipit cum repudiandae? Voluptates dolorum ipsa cumque dignissimos! Ut
-        deleniti cum, distinctio dolorem iure nisi, nesciunt atque ratione quis
-        ab, rerum quos? Delectus similique nostrum a temporibus, qui rem cumque
-        dolorem at voluptate voluptates quas eveniet architecto error assumenda.
-        Dolore perferendis incidunt voluptate enim! Ab consectetur officia culpa
-        expedita fuga dignissimos, deleniti quis error voluptate veritatis?
-        Aspernatur, doloribus qui minima consequatur incidunt magni vitae cum
-        numquam soluta hic, distinctio nam laudantium non repellat eligendi
-        mollitia, praesentium labore possimus iusto alias velit. Delectus iusto,
-        rem ullam amet quaerat veritatis, dolorem non, molestiae maxime dolorum
-        sunt dolores ipsa assumenda nobis earum maiores quibusdam a distinctio
-        placeat sit expedita accusantium fuga tempora voluptatem. Dignissimos
-        obcaecati esse nesciunt impedit eveniet eligendi repudiandae alias
-        quaerat, quos reprehenderit inventore aliquam totam sit accusantium
-        laborum voluptas ratione nisi pariatur voluptate vel tempore adipisci,
-        sunt earum recusandae! Omnis error facere voluptates magnam
-        exercitationem non minima! Doloribus neque dolores esse numquam
-        exercitationem quasi aperiam veritatis consequatur, ratione delectus
-        nihil assumenda magni non explicabo aspernatur voluptate facere omnis id
-        porro? Quos libero ducimus eos dolorem aliquid est recusandae iure
-        maiores voluptatibus saepe. Assumenda nesciunt tenetur accusamus
-        veritatis iure odit, dolorum vero, qui id quis a aliquid commodi
-        similique quidem ratione voluptatibus, nostrum in quia doloribus
-        consectetur reprehenderit. Voluptatem atque, soluta consequatur placeat
-        sapiente voluptate corporis numquam labore omnis facilis minus
-        perspiciatis quae nobis id, fugit odit. Dignissimos nulla expedita, nisi
-        provident, suscipit dolore quibusdam sequi facilis consequatur velit
-        ullam aliquid quasi labore a pariatur sit impedit dolorem soluta
-        similique. Tenetur, quae. Provident corrupti sapiente ratione nobis.
-        Magnam quibusdam rerum exercitationem perferendis, hic soluta eius,
-        corrupti harum similique quisquam odit autem ex odio molestias pariatur
-        saepe, sint nobis consequuntur doloribus facere. Officia voluptatum
-        quaerat debitis beatae nostrum, nulla maiores aliquid eveniet corporis
-        quasi voluptatem exercitationem ipsum rem totam numquam vel id corrupti
-        ab veniam expedita odit illum sed? Quod optio officia illo sed quas
-        neque minus ullam eum consequuntur? Eos optio numquam perspiciatis ab
-        officia nemo ea, atque voluptates, maiores, voluptatibus labore
-        recusandae quae modi. Neque est deserunt quam nesciunt sunt officiis
-        libero officia iste sint, veniam voluptatibus quisquam at. Magni numquam
-      </p>
+      <div>
+        <h1>Urnengarten-Goldenbek</h1>
+        <h2>Frieden findet man nur in den Wäldern.</h2>
+      </div>
+      <div className="container">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit
+          distinctio, quam officia odit neque harum consequatur aperiam
+          necessitatibus nisi voluptatum culpa, doloremque nemo, possimus
+          dignissimos exercitationem voluptas fugiat. Autem quaerat mollitia
+          asperiores cumque laudantium illum voluptas id fugit ab unde!
+          Exercitationem earum magnam sit, neque temporibus iste nulla assumenda
+          ratione totam obcaecati incidunt numquam fuga, dolorum enim! Vitae
+          animi, enim rerum laborum hic nostrum quisquam dolorum sint, culpa
+          molestias saepe? Minus voluptate obcaecati dolorum cumque impedit
+          consectetur officiis doloribus. Ratione autem corporis perspiciatis
+          consequuntur labore, laborum excepturi vitae qui neque amet et
+          temporibus voluptates cum provident odio atque ut quos itaque!
+          Consectetur repellat ut dolorum, atque natus voluptates quod odit
+          asperiores, totam fugiat autem, illum iste voluptate veniam optio
+          voluptatem? Nisi esse reprehenderit error repudiandae nam, modi ad
+          fugit. Beatae dolorem ipsa aperiam omnis vitae iure illo, dolorum,
+          sed, nesciunt reiciendis ab totam expedita recusandae voluptatem nulla
+          hic ullam magni enim! Iusto dolores quaerat voluptatem voluptas
+          accusamus dicta vero aspernatur neque odio nisi, provident labore
+          animi in consectetur quae id voluptate recusandae delectus eius
+          quisquam totam placeat ipsam. Unde consectetur assumenda tenetur
+          eligendi odio ab, maiores ipsam numquam quibusdam ipsum debitis
+          eveniet exercitationem incidunt laboriosam ea, vero, mollitia
+          accusantium obcaecati doloremque? Delectus neque accusantium
+          laudantium amet aspernatur qui natus maxime sunt vero numquam, culpa
+          aliquid excepturi quam tenetur, voluptatum doloribus nulla aperiam a
+          porro expedita suscipit esse? Sequi dignissimos ullam alias omnis, at
+          qui culpa vitae vel facilis eius, reprehenderit iure nam sapiente
+          repellat neque! At iusto deleniti fugiat suscipit cum repudiandae?
+          Voluptates dolorum ipsa cumque dignissimos! Ut deleniti cum,
+          distinctio dolorem iure nisi, nesciunt atque ratione quis Magnam
+          quibusdam rerum exercitationem perferendis, hic soluta eius, corrupti
+          harum similique quisquam odit autem ex odio molestias pariatur saepe,
+          sint nobis consequuntur doloribus facere. Officia voluptatum quaerat
+          debitis beatae nostrum, nulla maiores aliquid eveniet corporis quasi
+          voluptatem exercitationem ipsum rem totam numquam vel id corrupti ab
+          veniam expedita odit illum sed? Quod optio officia illo sed quas neque
+          minus ullam eum consequuntur? Eos optio numquam perspiciatis ab
+          officia nemo ea, atque voluptates, maiores, voluptatibus labore
+          recusandae quae modi. Neque est deserunt quam nesciunt sunt officiis
+          libero officia iste sint, veniam voluptatibus quisquam at. Magni
+          numquam
+        </p>
+      </div>
     </div>
   );
 }
